295c4be8bcf26bab3dcd9eaad6c6f73a070f8548
susuko <flokinyan@gmail.com>
2023-05-28 21:55:08 +0900

    add date

--- clock.js
+++ clock.js
@@ -1,19 +1,19 @@
 setInterval(() => {
     const date = new Date();
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-
-    document.getElementById('hourTen').src = `digits/${hours[0]}.png`;
-    document.getElementById('hourOne').src = `digits/${hours[1]}.png`;
-    document.getElementById('minuteTen').src = `digits/${minutes[0]}.png`;
-    document.getElementById('minuteOne').src = `digits/${minutes[1]}.png`;
-    document.getElementById('secondTen').src = `digits/${seconds[0]}.png`;
-    document.getElementById('secondOne').src = `digits/${seconds[1]}.png`;
+    updateDigits('y', String(date.getFullYear()).slice(-2).padStart(2, '0'));
+    updateDigits('M', String(date.getMonth() + 1).padStart(2, '0'));
+    updateDigits('d', String(date.getDate()).padStart(2, '0'));
+    updateDigits('H', String(date.getHours()).padStart(2, '0'));
+    updateDigits('m', String(date.getMinutes()).padStart(2, '0'));
+    updateDigits('s', String(date.getSeconds()).padStart(2, '0'));
 }, 1000);
 
-document.body.addEventListener('touchstart', () => {
-    if (!document.fullscreenElement) {
-        document.documentElement.requestFullscreen();
-    }
-});
+const updateDigits = (baseElementId, digits) => {
+    updateDigit(baseElementId + '_', digits[0]);
+    updateDigit('_' + baseElementId, digits[1]);
+};
+
+const updateDigit = (elementId, digit) =>
+    document.getElementById(elementId).src = `digits/${digit}.png`;
+
+document.body.addEventListener('touchstart', () => document.documentElement.requestFullscreen());
